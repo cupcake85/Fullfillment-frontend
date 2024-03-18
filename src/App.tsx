7897808727b61f452bd1bfbd23f3358d0153aed1
Cakeza007 <Wanphen9211@gmail.com>
2024-03-18 18:44:11 +0700

    nevpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Layout } from "antd";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Full from "../src/nevigation/Nev.tsx";
+import Warehouse from './nevigation/Warehouse.tsx'
+import Item from './nevigation/Item.tsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Fullfill() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Full />}>
+          <Route path="/Warehouse" element={<Warehouse />} />
+          <Route path="/Item" element={<Item />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default Fullfill;
---
+++ src/nevigation/Nev.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { UserOutlined, VideoCameraOutlined } from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import { useNavigate, Outlet } from "react-router-dom";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items = [
+  {
+    key: "/Warehouse",
+    icon: React.createElement(UserOutlined),
+    label: "Warehouse",
+  },
+  {
+    key: "/Item",
+    icon: React.createElement(VideoCameraOutlined),
+    label: "Item",
+  },
+];
+
+const App: React.FC = () => {
+  const navigate = useNavigate();
+
+  // const {
+  //   token: { colorBgContainer, borderRadiusLG },
+  // } = theme.useToken();
+
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider breakpoint="lg" collapsedWidth="0">
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["2"]}
+          items={items}
+          onClick={(e) => navigate(e.key)}
+        />
+      </Sider>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Layout>
+  );
+};
+
+export default App;
