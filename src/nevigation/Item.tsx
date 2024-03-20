aaaa4c8a814d4621ec390220544531b7d23ec00c
Kcin054 <nickpatiphat001@gmail.com>
2024-03-20 18:40:04 +0700

    modal insert page

--- src/nevigation/Item.tsx
+++ src/nevigation/Item.tsx
@@ -1,67 +1,36 @@
 import React, { useState } from "react";
 import {
-  Layout,
-  Flex,
   Button,
   ConfigProviderProps,
   Row,
   Col,
-  Table,
-  TableColumnsType,
-  Space,
+  Card,
   Modal,
+  TableColumnsType,
+  Table,
+  Space,
+  Input,
 } from "antd";
 import {
   DeleteFilled,
-  DownloadOutlined,
+  FileAddFilled,
   PlusCircleFilled,
   ProfileFilled,
 } from "@ant-design/icons";
 
 const Item = () => {
-  type SizeType = ConfigProviderProps["componentSize"];
-
+  //------------------------------------------------------------Table----------------------------------------------------------------------------------------
   interface DataType {
     key: React.Key;
-    sku: number;
+    sku: string;
     name: string;
-    store: string;
+    shop: string;
     note: string;
   }
 
-  const rowSelection = {
-    onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
-      console.log(
-        `selectedRowKeys: ${selectedRowKeys}`,
-        "selectedRows: ",
-        selectedRows
-      );
-    },
-    getCheckboxProps: (record: DataType) => ({
-      disabled: record.name === "Disabled User", // Column configuration not to be checked
-      name: record.name,
-    }),
-  };
-
-  const [size, setSize] = useState<SizeType>("large");
-
-  const [selectionType, setSelectionType] = useState<"radio" | "checkbox">(
+  const [selectionType, setSelectionType] = useState<"checkbox" | "radio">(
     "checkbox"
   );
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
 
   const columns: TableColumnsType<DataType> = [
     {
@@ -79,18 +48,22 @@ const Item = () => {
     },
     {
       title: "ร้านค้า",
-      dataIndex: "store",
+      dataIndex: "shop",
     },
     {
       title: "หมายเหตุ",
       dataIndex: "note",
     },
+    //------------------------------------------------------------edit modal----------------------------------------------------------------------------------------
     {
       title: "",
       key: "action",
       render: (_, record) => (
         <Space size="middle">
-          <Button onClick={showModal}>Edit</Button>
+          <Col>
+            <Button onClick={showModal} size="middle" style={{width:100, backgroundColor: "#262626", color:"#ffffff"}}>Edit</Button>
+            <Button size="middle" style={{width:100}}>Delete</Button>
+          </Col>
           <Modal
             style={{ backgroundColor: "#40a9ff" }}
             title="จัดการสินค้า"
@@ -105,7 +78,7 @@ const Item = () => {
                 <p>SKU</p>
               </Col>
               <Col>
-                <input></input>
+                <Input></Input>
               </Col>
             </Row>
             <Row align={"middle"}>
@@ -113,7 +86,7 @@ const Item = () => {
                 <p>ชื่อสินค้า</p>
               </Col>
               <Col>
-                <input></input>
+                <Input></Input>
               </Col>
             </Row>
             <Row align={"middle"}>
@@ -121,7 +94,7 @@ const Item = () => {
                 <p>หมายเหตุ</p>
               </Col>
               <Col>
-                <input></input>
+                <Input></Input>
               </Col>
             </Row>
             <Row align={"middle"}>
@@ -129,7 +102,7 @@ const Item = () => {
                 <p>ลูกค้า</p>
               </Col>
               <Col>
-                <input></input>
+                <Input></Input>
               </Col>
             </Row>
             <Row align={"middle"}>
@@ -137,65 +110,151 @@ const Item = () => {
                 <p>คำอธิบาย</p>
               </Col>
               <Col>
-                <input></input>
+                <Input></Input>
               </Col>
             </Row>
           </Modal>
-          <Button>Delete</Button>
         </Space>
       ),
     },
+    //------------------------------------------------------------edit modal----------------------------------------------------------------------------------------
   ];
 
   const data: DataType[] = [
     {
       key: "1",
+      sku: "",
       name: "",
-      sku: 32,
-      store: "",
+      shop: "",
       note: "",
     },
     {
       key: "2",
+      sku: "",
       name: "",
-      sku: 42,
-      store: "",
+      shop: "",
       note: "",
     },
     {
       key: "3",
+      sku: "",
       name: "",
-      sku: 32,
-      store: "",
+      shop: "",
       note: "",
     },
     {
       key: "4",
+      sku: "",
       name: "",
-      sku: 99,
-      store: "",
+      shop: "",
+      note: "",
+    },
+    {
+      key: "4",
+      sku: "",
+      name: "",
+      shop: "",
+      note: "",
+    },
+    {
+      key: "4",
+      sku: "",
+      name: "",
+      shop: "",
+      note: "",
+    },
+    {
+      key: "4",
+      sku: "",
+      name: "",
+      shop: "",
+      note: "",
+    },
+    {
+      key: "4",
+      sku: "",
+      name: "",
+      shop: "",
       note: "",
     },
   ];
 
+  const rowSelection = {
+    onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
+      console.log(
+        `selectedRowKeys: ${selectedRowKeys}`,
+        "selectedRows: ",
+        selectedRows
+      );
+    },
+    getCheckboxProps: (record: DataType) => ({
+      disabled: record.name === "Disabled User", // Column configuration not to be checked
+      name: record.name,
+    }),
+  };
+  //------------------------------------------------------------Table----------------------------------------------------------------------------------------
+  //------------------------------------------------------------Modal----------------------------------------------------------------------------------------
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen1, setIsModalOpen1] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const showModal1 = () => {
+    setIsModalOpen1(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleOk1 = () => {
+    setIsModalOpen1(false);
+  };
+
+  const handleCancel1 = () => {
+    setIsModalOpen1(false);
+  };
+  //------------------------------------------------------------Modal----------------------------------------------------------------------------------------
+
+  type SizeType = ConfigProviderProps["componentSize"];
+
+  const [size, setSize] = useState<SizeType>("large");
+
   return (
     <div>
-      <Row justify="center" style={{ backgroundColor: "#f0f0f0" }}>
-        <Col span={20}>
-          <Row justify="space-between">
-            <Col
-              style={{
-                marginLeft: 10,
-                marginTop: 10,
-                fontSize: "40px",
-                color: "#262626",
-              }}
-            >
-              <ProfileFilled /> จัดการ
+      <Row justify="center" align="middle">
+        <Card bordered={false} style={{ width: 1000, height: 700, margin: 12 }}>
+          {/* ---------------------------------------------------------------------------head-------------------------------------------------------------------------- */}
+          <Row
+            justify="center"
+            align="middle"
+            style={{ backgroundColor: "#f0f0f0" }}
+          >
+            <Col span={20}>
+              <Row justify="space-between" align="middle">
+                <Col
+                  style={{
+                    marginLeft: 10,
+                    marginTop: 10,
+                    fontSize: "40px",
+                    color: "#262626",
+                  }}
+                >
+                  <ProfileFilled /> จัดการ
+                </Col>
+              </Row>
             </Col>
-            <Col>
-              <br></br>
-              <br></br>
+          </Row>
+          {/* ---------------------------------------------------------------------------head-------------------------------------------------------------------------- */}
+
+          {/* ---------------------------------------------------------------------------content-------------------------------------------------------------------------- */}
+          <Row justify="end">
+            <Col style={{ margin: 10 }}>
               <Button
                 style={{ backgroundColor: "#262626" }}
                 type="primary"
@@ -211,26 +270,93 @@ const Item = () => {
                 shape="round"
                 icon={<PlusCircleFilled />}
                 size={size}
+                onClick={showModal1}
               >
                 สินค้า
               </Button>
+              {/* ---------------------------------------------------------------------------modal-------------------------------------------------------------------------- */}
+              <Modal
+                title="เพิ่มสินค้า"
+                open={isModalOpen1}
+                onOk={handleOk1}
+                onCancel={handleCancel1}
+                width={600}
+              >
+                <Row style={{ fontSize: "20px" }}>
+                  <FileAddFilled />
+                  <p style={{ marginLeft: 10 }}>รายละเอียดสินค้า</p>
+                </Row>
+                <Row align={"middle"}>
+                  <Col span={5}>
+                    <p style={{ marginLeft: 30 }}>รหัสสินค้า</p>
+                  </Col>
+                  <Col>
+                    <Input></Input>
+                  </Col>
+                </Row>
+                <Row align={"middle"}>
+                  <Col span={5}>
+                    <p style={{ marginLeft: 30 }}>ชื่อสินค้า</p>
+                  </Col>
+                  <Col>
+                    <Input></Input>
+                  </Col>
+                </Row>
+                <Row align={"middle"}>
+                  <Col span={5}>
+                    <p style={{ marginLeft: 30 }}>หมวดหมู่</p>
+                  </Col>
+                  <Col>
+                    <Input></Input>
+                  </Col>
+                </Row>
+                <Row align={"middle"}>
+                  <Col span={5}>
+                    <p style={{ marginLeft: 30 }}>หน่วย</p>
+                  </Col>
+                  <Col>
+                    <Input></Input>
+                  </Col>
+                </Row>
+                <Row align={"middle"}>
+                  <Col span={5}>
+                    <p style={{ marginLeft: 30 }}>บาร์โค้ด</p>
+                  </Col>
+                  <Col>
+                    <Input></Input>
+                  </Col>
+                </Row>
+                <Row align={"middle"}>
+                  <Col span={5}>
+                    <p style={{ marginLeft: 30 }}>Tag สินค้า</p>
+                  </Col>
+                  <Col>
+                    <Input></Input>
+                  </Col>
+                </Row>
+              </Modal>
+              {/* ---------------------------------------------------------------------------Modal-------------------------------------------------------------------------- */}
             </Col>
           </Row>
-        </Col>
-      </Row>
-      <Row justify="center">
-        <Col span={20}>
-          <br></br>
-          <Table
-            rowSelection={{
-              type: selectionType,
-              ...rowSelection,
-            }}
-            columns={columns}
-            dataSource={data}
-            scroll={{ x: 1000 }}
-          />
-        </Col>
+          {/* ---------------------------------------------------------------------------Table-------------------------------------------------------------------------- */}
+          <Row justify="center">
+            <Col span={20}>
+              <br></br>
+              <Table
+                rowSelection={{
+                  type: selectionType,
+                  ...rowSelection,
+                }}
+                columns={columns}
+                dataSource={data}
+                pagination={{ defaultCurrent: 6, total: 500 }}
+                scroll={{ x: 400, y: 350 }}
+              />
+            </Col>
+          </Row>
+          {/* ---------------------------------------------------------------------------Table-------------------------------------------------------------------------- */}
+          {/* ---------------------------------------------------------------------------content-------------------------------------------------------------------------- */}
+        </Card>
       </Row>
     </div>
   );
