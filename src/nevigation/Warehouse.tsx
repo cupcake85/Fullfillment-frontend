10ed600a17d49df0671c133037806ff2c8628174
isPeang <soonyata92@gmail.com>
2024-03-22 16:32:01 +0700

    click-checkbox-table and history

--- src/nevigation/Warehouse.tsx
+++ src/nevigation/Warehouse.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, Modal, Table, Input, Divider, Form } from 'antd';
+import { Button, Card, Modal, Table, Input, Divider, Form, InputNumber } from 'antd';
 import '../warehouse.css';
 import axios from 'axios';
 import { useForm } from 'antd/es/form/Form';
+import ItemInput from "../components/plus-modal";
 
 type FieldType = {
   username?: string;
@@ -14,28 +15,66 @@ interface DataType {
   key: React.Key;
   sku: string;
   name: string;
-  detail: string;
-  store: string;
-  storage: string;
+  details: string;
   quantity: number;
 }
 
 const Warehouse = () => {
   const [warehousedata, setWarehouse] = useState([]);
-  const [selectionType] = useState<'checkbox' | 'radio'>('checkbox');
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [addModal, setAddmodal] = useState(false);
   const [form] = useForm();
+  const [selectedRows, setSelectedRows] = useState<DataType[]>([]); // เพิ่ม state เก็บข้อมูลที่เลือกไว้
+  const [isModalHistory, setIsModalHistory] = useState(false);
+  const [historyId, setHistoryId] = useState();
+  const [getHistory, setHistory] = useState([]);
+
+
   const showModal = () => { setIsModalVisible(true); };
   const handleCancel = () => { setIsModalVisible(false) };
+  const showAddModal = () => { setAddmodal(true); };
+  const addCancel = () => { setAddmodal(false) };
+  const showModalh = () => {
+    setIsModalHistory(true);
+  };
+  const handleCancelh = () => {
+    setIsModalHistory(false);
+  };
+
 
   useEffect(() => {
     getWarehouse();
   }, []);
 
+  // const data: DataType[] = [
+  //   {
+  //     key: '1',
+  //     sku: 'John Brown',
+  //     name: 'John Brown',
+  //     details: 'New York No. 1 Lake Park',
+  //     quantity: 100,
+  //   },
+  //   {
+  //     key: '1',
+  //     sku: 'John Brown',
+  //     name: 'John Brown',
+  //     details: 'New York No. 1 Lake Park',
+  //     quantity: 100,
+  //   },
+  //   {
+  //     key: '1',
+  //     sku: 'John Brown',
+  //     name: 'John Brown',
+  //     details: 'New York No. 1 Lake Park',
+  //     quantity: 100,
+  //   },
+  // ]
+
+
   const columns = [
     {
       title: '#',
-      dataIndex: 'key',
+      dataIndex: 'id',
     },
     {
       title: 'SKU',
@@ -67,60 +106,84 @@ const Warehouse = () => {
     }
   ];
 
-  // const data: DataType[] = [
-  //   {
-  //     key: '1',
-  //     sku: 'HW001',
-  //     name: "House",
-  //     detail: "ksdpksp",
-  //     store: "pdlspdl",
-  //     storage: "string",
-  //     quantity: 100
-  //   },
-  //   {
-  //     key: '2',
-  //     sku: 'HW001',
-  //     name: "House",
-  //     detail: "ksdpksp",
-  //     store: "pdlspdl",
-  //     storage: "string",
-  //     quantity: 100
-  //   },
-  //   {
-  //     key: '3',
-  //     sku: 'HW001',
-  //     name: "House",
-  //     detail: "ksdpksp",
-  //     store: "pdlspdl",
-  //     storage: "string",
-  //     quantity: 100
-  //   },
-  // ];
-
-  const rowSelection = {
-    type: selectionType,
-    onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
-      console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  const addColumns = [
+    {
+      title: '#',
+      dataIndex: 'id',
     },
-    getCheckboxProps: (record: DataType) => ({
-      name: record.name,
-    }),
-  };
+    {
+      title: 'SKU',
+      dataIndex: 'sku',
+    },
+    {
+      title: 'ชื่อสินค้า',
+      dataIndex: 'name',
+    },
+    {
+      title: 'รายละเอียด',
+      dataIndex: 'details',
+    },
+    {
+      title: 'คงเหลือ',
+      key: 'quantity',
+      dataIndex: 'quantity'
+    },
+    {
+      tilte: 'เพิ่ม/ลด',
+      render: (value: any, record: any) => {
+        let quantity = record.quantity
+        return <InputNumber
+          value={quantity}
+          onChange={(value) => quantity = value} />
+      }
+    }
+  ];
+
+  const columnh = [
+    {
+      title: "Id",
+      dataIndex: "id",
+    },
+    {
+      title: "ชื่อสินค้า",
+      dataIndex: "note",
+    },
+    {
+      title: "Date",
+      dataIndex: "outDate",
+    },
+    {
+      title: "รายละเอียด",
+      dataIndex: "remark",
+    },
+    {
+      title: "คงเหลือ",
+      key: "quantity",
+      dataIndex: "quantity",
+    },
+  ];
 
   const getWarehouse = async () => {
     const request = await axios.get('http://192.168.2.57:3000/items/')
-    console.log('request', request)
-    setWarehouse(request.data.data)
+    const sortedData = request.data.data.sort((a: any, b: any) => {
+      // เรียงลำดับตาม id จากน้อยไปหามาก
+      if (a.id < b.id) return -1;
+      if (a.id > b.id) return 1;
+      return 0;
+    });
+    console.log('request get', request)
+    // setWarehouse(request.data.data) ยังไม่ sort
+    setWarehouse(sortedData)
   }
 
   const updateWarehouse = async (id: number, value: any) => {
     const request = await axios.put('http://192.168.2.57:3000/items/' + id, value)
-    console.log('request', request)
+    console.log('request put', request)
     getWarehouse();
   }
 
   function onokfunction() {
-    const a = form.getFieldsValue(['id', /*'sku', 'details', 'name',*/ 'quantity'])
+    const a = form.getFieldsValue(['id', 'sku', 'details', 'name', 'quantity'])
     updateWarehouse(a.id, a)
     setIsModalVisible(false) //ปิด Modal ทิ้ง
   }
@@ -136,9 +199,37 @@ const Warehouse = () => {
     })
   }
 
+  const clickAdd = () => {
+    showAddModal()
+  };
+
   const historyClick = (value: any) => {
-    showModal();
-  }
+    showModalh();
+    History(value.id)
+  };
+
+
+  const rowSelection = {
+    onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
+      console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+      setSelectedRows(selectedRows); // เมื่อมีการเลือกแถวใหม่ให้เซ็ตค่า state
+    }
+  };
+
+  const History = async (id: number) => {
+    try {
+      const request = await axios.get("http://192.168.2.57:3000/history/");
+      // setHistory(request.data.data);
+      const history = request.data.data.filter((item: any) => {
+        return item.item.id === id
+      })
+      setHistory(history)
+
+      console.log(history);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
@@ -151,7 +242,9 @@ const Warehouse = () => {
           borderRadius: 20
         }}>
         <div >
-          <Button style={{ backgroundColor: '#979a9c', color: 'white', borderRadius: '17px', marginBottom: '15px' }}>เพิ่ม</Button>{' '}
+          <Button onClick={() => clickAdd()}
+            style={{ backgroundColor: '#979a9c', color: 'white', borderRadius: '17px', marginBottom: '15px' }}
+          >เพิ่ม</Button>{' '}
           <Button style={{ backgroundColor: '#979a9c', color: 'white', borderRadius: '17px', marginBottom: '15px' }}>ลด</Button>
         </div>
 
@@ -159,6 +252,7 @@ const Warehouse = () => {
 
         <Table
           rowSelection={rowSelection}
+          rowKey={"id"} //ใช้ id แยกข้อมูลที่มาจาก array แล้ว
           pagination={{ defaultCurrent: 1 }} //แถบเลือกหน้า
           style={{ backgroundColor: '#e4e5e5' }}
           dataSource={warehousedata}
@@ -168,7 +262,7 @@ const Warehouse = () => {
         />
       </Card>
 
-      <Modal title="Basic Modal" open={isModalVisible} onOk={onokfunction} onCancel={handleCancel}>
+      <Modal title="แก้ไขสินค้า" open={isModalVisible} onOk={onokfunction} onCancel={handleCancel}>
         <Form
           name="basic"
           form={form}
@@ -209,10 +303,33 @@ const Warehouse = () => {
           </Form.Item>
         </Form>
       </Modal>
+
+      <Modal title="จัดการสินค้า" open={addModal} onCancel={addCancel} footer={null} >
+        <Table
+          style={{ backgroundColor: '#e4e5e5' }}
+          columns={addColumns}
+          dataSource={selectedRows}
+        />
+      </Modal>
+
+      <Modal
+        title="Basic Modal"
+        open={isModalHistory}
+        onOk={onokfunction}
+        onCancel={handleCancelh}
+        footer={null}
+      >
+        <Table
+          // pagination={{ defaultCurrent: 1 }}
+          style={{ backgroundColor: "#e4e5e5" }}
+          dataSource={getHistory}
+          columns={columnh}
+          scroll={{ x: 700 }} //ความกว้าง scroll ได้ 1200
+        />
+      </Modal>
+
     </>
   );
 };
 
 export default Warehouse;
-
-
