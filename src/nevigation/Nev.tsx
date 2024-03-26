72cf00788e453abe619c6ea237c618cdca9b977b
isPeang <soonyata92@gmail.com>
2024-03-26 16:50:10 +0700

    merge

--- src/nevigation/Nev.tsx
+++ src/nevigation/Nev.tsx
@@ -92,11 +92,7 @@ const App: React.FC = () => {
               onClick={(e) => onClick(e.key)}
             />
           </Sider>
-<<<<<<< HEAD
-          <div style={{ width: "100vw", height: "100vh" }}>
-=======
           <div style={{ width: "100vw", height: "100vh" ,overflow:'scroll'}}>
->>>>>>> origin
             <Outlet />
           </div>
         </Layout>
