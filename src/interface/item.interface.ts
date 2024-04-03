fdc00227223eb295846bed128dfb095bd3b25810
isPeang <soonyata92@gmail.com>
2024-04-03 11:39:38 +0700

    multiple submit function + day.js + show sku in history class + interface

---
+++ src/interface/item.interface.ts
@@ -0,0 +1,34 @@
+export interface Iitem {
+    id: number,
+    sku: string,
+    name:string,
+    details?: string,
+    quantity: number,
+    stores: IStores,
+    history: IHistory[]
+}
+
+export interface IStores {
+  id: number,
+  name: string,
+  shipperCode?: string,
+  shipperName?: string,
+  zipCode: string,
+  phoneNumber: string,
+  email: string,
+  isDelete: boolean
+}
+
+export interface IResult<T>{
+  data:T
+}
+
+export interface IHistory {
+  id: number;
+  lot: string;
+  order: string;
+  outDate: string;
+  quantity: number;
+  remark: string;
+  item: Iitem;
+}
